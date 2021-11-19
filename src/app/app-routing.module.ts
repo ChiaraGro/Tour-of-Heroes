3131d77cf8e3a4cc93e8ed06cc1f83a76f2a2564
Chiara Grottini <chiaragrottini@gmail.com>
2021-11-19 22:03:01 +0100

    add heroes link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { NgModule } from '@angular/core';
@@ -6,6 +7,8 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {path: 'heroes', component: HeroesComponent},
   {path: 'dashboard', component: DashboardComponent},
+  {path: 'detail/:id', component: HeroDetailComponent},
+  //The colon (:) in the path indicates that :id is a placeholder for a specific hero id.
 
   {path: '', redirectTo:'/dashboard', pathMatch:'full'}
   //To make the application navigate to the dashboard automatically
